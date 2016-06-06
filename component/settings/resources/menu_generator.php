7dace6d4763321ad6bdf9709380568983ab96d57
unforgiven1127 <munir_anameric@hotmail.com>
2016-06-06 14:55:02 -0700

    test

--- component/settings/resources/menu_generator.php
+++ component/settings/resources/menu_generator.php
@@ -406,7 +406,7 @@ $asArray = array();
   array (
     'name' => 'EXPERTISE',
     'link' => 'http://www.slate.co.jp/expertise/',
-    'legend' => 'What we specialise in',
+    'legend' => 'What we specialise In',
     'icon' => '',
     'target' => '',
     'uid' => '',
@@ -719,7 +719,7 @@ $asArray['jp'] = array (
   array (
     'name' => 'EXPERTISE',
     'link' => 'http://www.slate.co.jp/expertise/',
-    'legend' => 'What we specialise in',
+    'legend' => 'What we Specialise in',
     'icon' => '',
     'target' => '',
     'uid' => '',
