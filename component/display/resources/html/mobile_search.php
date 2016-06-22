d761671f3180e0c355e1d7398ed0ecc7e0523c15
unforgiven1127 <munir_anameric@hotmail.com>
2016-06-22 15:58:44 -0700

    quick search

--- component/display/resources/html/mobile_search.php
+++ component/display/resources/html/mobile_search.php
@@ -25,8 +25,8 @@
     <form action='https://jobs.slate.co.jp/index.php5?uid=153-160&ppa=ppal&ppt=job&ppk=0&pg=ajx' method='post'>
        <table style='margin-top:-50px;'>
         <tr >
-          <td class='width'>
-            <input style="width:900px;" placeholder='Keywords' type='text' class='form-control' name='keyword' id='keyword'>
+          <td style="width:100%;" class='width'>
+            <input style="width:100%;" placeholder='Keywords' type='text' class='form-control' name='keyword' id='keyword'>
           </td>
         </tr>
         <tr>
