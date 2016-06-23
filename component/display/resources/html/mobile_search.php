0cc29244a16254487821fb726d555f9a39db8393
unforgiven1127 <munir_anameric@hotmail.com>
2016-06-22 18:52:37 -0700

    mobile

--- component/display/resources/html/mobile_search.php
+++ component/display/resources/html/mobile_search.php
@@ -50,7 +50,7 @@
               <img align="right" style='width: 100px; margin-right:10px;' src="http://jobs.slate.co.jp/component/display/resources/jobboard/pictures/eng.png" title="English">
             </a>
             <a href="" >
-              <img align="right" style='margin-left:10px; margin-right:10px; width: 100px;' src="http://jobs.slate.co.jp/component/display/resources/jobboard/pictures/japan.png" title="日本語">
+              <img align="right" style='margin-left:10px; margin-right:50px; width: 100px;' src="http://jobs.slate.co.jp/component/display/resources/jobboard/pictures/japan.png" title="日本語">
             </a>
           </td>
         </tr>
