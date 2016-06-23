24595a44098e9f3d5b805e171524c01118b2457f
unforgiven1127 <munir_anameric@hotmail.com>
2016-06-23 16:07:12 -0700

    mobile

--- component/display/resources/html/mobile_search.php
+++ component/display/resources/html/mobile_search.php
@@ -163,19 +163,19 @@
                 <i><p style='font-size: 8pt; margin-top: 0em; margin-bottom: 0em;'><b style='font-size: 8pt;'><br>Requirements: <br></b>".nl2br($value['requirements'])."</p></i>
                 <table>
                   <tr>
-                    <td><i><p class='less' id='".$i."_hidden' style='font-size: 8pt; margin-top: 1em; margin-bottom: 1em;'>
-                  <a href='#".$i."_move' style='font-size: 10pt; font-style: oblique;'>- Click for less</a>
-                </p></i>
                     <td>
-                    
-                    <td><i><p class='less' id='".$i."_hidden' style='font-size: 8pt; margin-top: 1em; margin-bottom: 1em;'>
-                  <a href='#".$i."_move' style='font-size: 10pt; font-style: oblique;'>- Click for less</a>
-                </p></i>
+                      <i><p class='less' id='".$i."_hidden' style='font-size: 8pt; margin-top: 1em; margin-bottom: 1em;'>
+                        <a href='#".$i."_move' style='font-size: 10pt; font-style: oblique;'>- Click for less</a>
+                      </p></i>
+                    <td>
+
+                    <td>
+                      <i><p class='less' align='right' id='".$i."_hidden' style='font-size: 8pt; margin-top: 1em; margin-bottom: 1em;'>
+                        <a href='#".$i."_move' style='font-size: 10pt; font-style: oblique;'># Apply</a>
+                      </p></i>
                     <td>
                   </tr>
                 </table>
-                
-                
               </td>
             </tr>";
             $i++;
