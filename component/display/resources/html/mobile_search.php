cf25a3a8277782bddc0e669998c94f2cb8cf6039
unforgiven1127 <munir_anameric@hotmail.com>
2016-06-22 21:36:14 -0700

    mobile

--- component/display/resources/html/mobile_search.php
+++ component/display/resources/html/mobile_search.php
@@ -92,7 +92,7 @@
         <tr>
           <td style='padding-top:15px;'>
             <center>
-              <select style="width:100%; height: 30px; font-size: 8pt; font-weight: bold; background-color:rgba(0,0,0,0) !important;" class="form-control" name="industry" id="industry">
+              <select style="width:100%; height: 30px; font-size: 10pt; font-weight: bold; background-color:rgba(0,0,0,0) !important;" class="form-control" name="industry" id="industry">
                 <option value='' >Select Industry</option>
                 <?php foreach($industries as $key => $value){
                   echo "<option value='$key' >$value</option>";
