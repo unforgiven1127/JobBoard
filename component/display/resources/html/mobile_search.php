d1f9bf71e4012f18f9eec330be6ec0849df44644
unforgiven1127 <munir_anameric@hotmail.com>
2016-06-23 16:05:17 -0700

    mobile

--- component/display/resources/html/mobile_search.php
+++ component/display/resources/html/mobile_search.php
@@ -161,12 +161,20 @@
               <td style='padding-left:5%; padding-right:5%; width:90% !important;'>
                 <i><p style='font-size: 8pt; margin-top: 0em; margin-bottom: 0em;'><b style='font-size: 8pt;'>Description: <br></b>".nl2br($value['position_desc'])."</p></i>
                 <i><p style='font-size: 8pt; margin-top: 0em; margin-bottom: 0em;'><b style='font-size: 8pt;'><br>Requirements: <br></b>".nl2br($value['requirements'])."</p></i>
-                <div><i><p class='less' id='".$i."_hidden' style='font-size: 8pt; margin-top: 1em; margin-bottom: 1em;'>
+                <table>
+                  <tr>
+                    <td><i><p class='less' id='".$i."_hidden' style='font-size: 8pt; margin-top: 1em; margin-bottom: 1em;'>
                   <a href='#".$i."_move' style='font-size: 10pt; font-style: oblique;'>- Click for less</a>
                 </p></i>
-                <i><p class='less' id='".$i."_hidden' style='font-size: 8pt; margin-top: 1em; margin-bottom: 1em;'>
+                    <td>
+                    
+                    <td><i><p class='less' id='".$i."_hidden' style='font-size: 8pt; margin-top: 1em; margin-bottom: 1em;'>
                   <a href='#".$i."_move' style='font-size: 10pt; font-style: oblique;'>- Click for less</a>
-                </p></i></div>
+                </p></i>
+                    <td>
+                  </tr>
+                </table>
+                
                 
               </td>
             </tr>";
