5dcb44e920cabe6119aea8e8de31d293bf16490b
unforgiven1127 <munir_anameric@hotmail.com>
2016-06-22 18:42:38 -0700

    mobile

--- component/display/resources/html/mobile_search.php
+++ component/display/resources/html/mobile_search.php
@@ -40,7 +40,7 @@
   <body>
 
     <form action='https://jobs.slate.co.jp/index.php5?uid=153-160&ppa=ppal&ppt=job&ppk=0&pg=ajx' method='post'>
-       <table bgcolor="white" style='width:100%; background: url("http://jobs.slate.co.jp/component/display/resources/jobboard/pictures/bg_s.png") no-repeat;'>
+       <table bgcolor="white" style='width:100%; background: url("http://jobs.slate.co.jp/component/display/resources/jobboard/pictures/bg_s.png") no-repeat center center fixed;'>
         <tr>
           <td>
             <a href="http://www.slate.co.jp">
