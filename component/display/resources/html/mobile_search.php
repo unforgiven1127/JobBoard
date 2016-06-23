8d61f90277f0e95c293673f62912af2573b32aa4
unforgiven1127 <munir_anameric@hotmail.com>
2016-06-23 16:28:34 -0700

    mobile

--- component/display/resources/html/mobile_search.php
+++ component/display/resources/html/mobile_search.php
@@ -84,7 +84,7 @@
           list-style-type: disc;
           list-style-position: inside;
           padding-left: 10px;
-          text-indent: -1em;
+          text-indent: -3px;
         }
 
     </style>
