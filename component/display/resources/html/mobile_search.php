1f443c792aad6ce52415ff45dcf810d52e924379
unforgiven1127 <munir_anameric@hotmail.com>
2016-06-23 15:16:25 -0700

    mobile

--- component/display/resources/html/mobile_search.php
+++ component/display/resources/html/mobile_search.php
@@ -57,16 +57,16 @@
 
       ::-webkit-input-placeholder
       {
-         margin-left:15px;
+         margin-left:20px;
         }
         :-moz-placeholder { /* older Firefox*/
-         margin-left:15px;
+         margin-left:20px;
         }
         ::-moz-placeholder { /* Firefox 19+ */
-         margin-left:15px;
+         margin-left:20px;
         }
         :-ms-input-placeholder {
-         margin-left:15px;
+         margin-left:20px;
         }
 
     </style>
