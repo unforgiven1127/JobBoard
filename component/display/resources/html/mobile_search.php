0078f85b1899e55dd18faf50ee7550dc2fcee94d
unforgiven1127 <munir_anameric@hotmail.com>
2016-06-23 16:17:28 -0700

    mobile

--- component/display/resources/html/mobile_search.php
+++ component/display/resources/html/mobile_search.php
@@ -150,8 +150,8 @@
             echo "<tr id='".$i."_move' class='border_bottom' style='width:90% !important; ".$color." '>
                     <td style='padding-left:5%; padding-right:5%; width:90% !important;'>
                       <b><p style='font-size: 10pt; margin-top: 1em; margin-bottom: 0em;'>".$value['position_title']."</p></b>
-                      <b><p style='font-size: 10pt; margin-top: 2px; margin-bottom: 0em;'>".$value['location']."</p></b>
-                      <b><p style='font-size: 10pt; margin-top: 2px; margin-bottom: 0em;'>".$value['name']."</p></b>
+                      <b><p style='font-size: 10pt; margin-top: 3px; margin-bottom: 0em;'>".$value['location']."</p></b>
+                      <b><p style='font-size: 10pt; margin-top: 3px; margin-bottom: 0em;'>".$value['name']."</p></b>
                       <b><p id='".$i."' class='more' style='font-size: 10pt; margin-top: 1em;'>
                         <a style='font-size: 10pt; font-style: oblique;'>+ Click for more</a>
                       </p></b>
