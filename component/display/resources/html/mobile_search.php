946e58320118af7d3d530e2915b8b323487610d0
unforgiven1127 <munir_anameric@hotmail.com>
2016-06-23 20:27:52 -0700

    mobile

--- component/display/resources/html/mobile_search.php
+++ component/display/resources/html/mobile_search.php
@@ -38,24 +38,7 @@
 
         });
 
-        $(".apply").click(function(){
-            var id = this.id;
-            var id = id.split('_');
-
-            var position_id = id[0];
-            var url = id[1];
-
-            var nameID = position_id+"_name";
-            var contactID = position_id+"contact";
-
-            var name =document.getElementById(nameID).value;
-            var contact =document.getElementById(contactID).value;
-
-            var finalUrl = url+"&name="+name+"&contact="contact;
-
-            alert(finalUrl);
-
-        });
+        
     });
 
     </script>
