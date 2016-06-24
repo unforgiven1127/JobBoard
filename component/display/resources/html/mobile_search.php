4a9c40c819d5d481586b85059114bb7b525fd18c
unforgiven1127 <munir_anameric@hotmail.com>
2016-06-23 20:17:11 -0700

    mobile

--- component/display/resources/html/mobile_search.php
+++ component/display/resources/html/mobile_search.php
@@ -207,7 +207,7 @@
                         <tr>
                           <td style='width:50%;'>
                             <i><p class='less' id='".$i."_hidden' style='font-size: 8pt; margin-top: 1em; margin-bottom: 1em;'>
-                              <a href='".$value['applyUrl']."'  style='font-size: 10pt; font-style: oblique;'
+                              <a href=''  style='font-size: 10pt; font-style: oblique;'
                               onclick='this.href='".$value['applyUrl']."&name='+document.getElementById('fullName').value'&contact='+document.getElementById('contactInfo').value'># ".$apply."</a>
                             </p></i>
                           <td>
