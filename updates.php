4f9952eec08b2eeb7596d0d1949669bd66c37816
unforgiven1127 <munir_anameric@hotmail.com>
2016-06-27 19:52:16 -0700

    update

--- updates.php
+++ updates.php
@@ -288,7 +288,7 @@ foreach ($array as $key => $value)
     	$slistemQuery = "SELECT * FROM positions p WHERE p.external_key = '".$value."'";
 
     	$positionData = mysql_query($slistemQuery);
-var_dump($positionData);
+var_dump($slistemQuery);
 echo'<br><br>';
     	array_push($positionArrayJB,$positionData);
     }
