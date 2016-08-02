001b9e69be201bee3cfa93dcfa4fb46e5ef68c59
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-01 21:24:21 -0700

    client login

--- component/display/resources/html/client_main_page.php
+++ component/display/resources/html/client_main_page.php
@@ -14,9 +14,30 @@
 
 </script>
 <style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    width: 200px;
+    background-color: #f1f1f1;
+}
 
+li a {
+    display: block;
+    color: #000;
+    padding: 8px 16px;
+    text-decoration: none;
+}
 
+li a.active {
+    background-color: #4CAF50;
+    color: white;
+}
 
+li a:hover:not(.active) {
+    background-color: #555;
+    color: white;
+}
 </style>
 
   </head>
@@ -27,7 +48,24 @@
   <table style="width: 100%; margin-left: -10px;">
     <tr>
       <td valign="middle" align="middle" class="half" style=" width: 50%;">
-      CLIENT MAIN PAGE
+        <table>
+          <tr>
+            <td>
+              <ul>
+                <li><a class="active" href="#home">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+              </ul>
+            </td>
+          </tr>
+        <table>
+        <table>
+          <tr>
+            <td>
+            </td>
+          </tr>
+        <table>
       </td>
     </tr>
   </table>
