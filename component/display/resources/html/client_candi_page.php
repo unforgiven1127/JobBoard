23885ef3bf3d976d8cb8f7a0f3fb54048dec7491
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-07 18:12:01 -0700

    client side

--- component/display/resources/html/client_candi_page.php
+++ component/display/resources/html/client_candi_page.php
@@ -29,7 +29,7 @@
   <table >
     <tr>
       <td valign="top" style='width: 750px;' >
-        <table class='innerTable' style='width: 750px;' >
+        <table border="1" class='innerTable' style='width: 750px;' >
           <tr style='background-color: #EEEEEE;'>
             <th><img src="/common/pictures/list_action.png" /></th>
             <th>ID</th>
