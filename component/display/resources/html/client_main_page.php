ce9332ed24e58f6a217afe6aea8edbbd93c09821
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-07 17:22:11 -0700

    client side

--- component/display/resources/html/client_main_page.php
+++ component/display/resources/html/client_main_page.php
@@ -146,6 +146,112 @@ html, body {
               </ul>
             </td>
           </tr>
+          <tr>
+            <td>
+              <table style="width:105%; margin-left:-5px;">
+                <tbody><tr class="closeTR">
+                  <th>
+                      <div class="label  hidden"><b>Candidate</b></div>
+                  </th>
+                  <td>
+                      <div class="field">
+                        <input type="text" name="candidate" placeholder="ID  or  lastname, firstname" value="" onblur="if($(this).val().trim().length == 0)
+                          { $(this).val($(this).attr('data-default'));}
+                          else
+                          {
+                            asValue = $(this).val().trim().split(',');
+                            if(asValue.length > 2)
+                              return alert('There should be only 1 comma to separate the lastname and the firstname.');
+
+                            if(asValue.length == 2)
+                            return true;
+
+                          }">
+                      </div>
+                    </td>
+                  </tr>
+                  <tr class="closeTR">
+                      <th>
+                        <div class="label  hidden"><b>Contacts</b></div>
+                      </th>
+                      <td>
+                        <div class="field">
+                          <input type="text" name="contact" class="defaultText" data-default="Contact" value="Contact" onfocus="if($(this).val() == $(this).attr('data-default')){ $(this).val(''); $(this).removeClass('defaultText'); }" onblur="if($(this).val().trim().length == 0){ $(this).val($(this).attr('data-default')); $(this).addClass('defaultText');}">
+                        </div>
+                      </td>
+                  </tr>
+                  <tr class="closeTR">
+                    <th>
+                      <div class="label  hidden"><b>Company</b></div>
+                    </th>
+                    <td>
+                      <div class="field">
+                        <input type="text" name="company" class="defaultText" data-default="Company" value="Company" onfocus="if($(this).val() == $(this).attr('data-default')){ $(this).val(''); $(this).removeClass('defaultText'); }" onblur="if($(this).val().trim().length == 0){ $(this).val($(this).attr('data-default')); $(this).addClass('defaultText');}">
+                      </div>
+                    </td>
+                  </tr>
+                  <tr class="closeTR">
+                    <th>
+                      <div class="label  hidden"><b>Department</b></div>
+                    </th>
+                    <td>
+                      <div class="field">
+                        <input type="text" name="department" class="defaultText" data-default="Department" value="Department" onfocus="if($(this).val() == $(this).attr('data-default')){ $(this).val(''); $(this).removeClass('defaultText'); }" onblur="if($(this).val().trim().length == 0){ $(this).val($(this).attr('data-default')); $(this).addClass('defaultText');}">
+                      </div>
+                    </td>
+                  </tr>
+                  <tr class="closeTR">
+                    <th>
+                      <div class="label  hidden"><b>Position</b></div>
+                    </th>
+                    <td>
+                      <div class="field">
+                        <input type="text" name="position" class="defaultText" data-default="Position ID or title" value="Position ID or title" onfocus="if($(this).val() == $(this).attr('data-default')){ $(this).val(''); $(this).removeClass('defaultText'); }" onblur="if($(this).val().trim().length == 0){ $(this).val($(this).attr('data-default')); $(this).addClass('defaultText');}" onchange="$('#qs_pos_status').val(''); ">
+                        <input type="hidden" id="qs_pos_status" name="position_status" value="">
+                      </div>
+                    </td>
+                  </tr>
+                  <tr class="closeTR">
+                    <th>
+                      <div class="label  hidden"><b>Keyword</b></div>
+                    </th>
+                    <td>
+                      <div class="field">
+                        <input type="text" name="keyword" class="defaultText" data-default="Keyword" value="Keyword" onfocus="if($(this).val() == $(this).attr('data-default')){ $(this).val(''); $(this).removeClass('defaultText'); }" onblur="if($(this).val().trim().length == 0){ $(this).val($(this).attr('data-default')); $(this).addClass('defaultText');}">
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colspan="2">
+                <div class="hidden option">Search options</div>
+
+      <div class="hidden option"><div class="label  hidden">wide search</div><div class="field">
+      <input type="checkbox" name="qs_wide" checked="checked"> (contains the string)</div></div>
+
+      <div class="hidden option"><div class="label  hidden">Get lucky</div><div class="field">
+      <input type="checkbox" name="qs_super_wide"> (lastname OR firstname)</div></div>
+
+      <div class="hidden option"><div class="label  hidden">name format</div><div class="field">
+      <select name="qs_name_format">
+        <option value="lastname">Lastname, Firstname</option>
+        <option value="firstname">Firstname, Lastname</option>
+        <option value="none" selected="selected">Indifferent (slower/wider search)</option>
+      </select>
+      </div></div>
+
+      <div class="hidden option" style="margin-top: 15px;"><a class="floatLeft" href="javascript:;" onclick="$(this).closest('td').find('> div:not(.option_link)').toggle(0); $('.closeTR').toggle(0);">&nbsp;apply&nbsp;</a></div>
+
+
+    <div class="qs_action_row">
+    <a class="floatLeft" href="javascript:;" onclick="$(this).closest('td').find('> div:not(.option_link)').toggle(0); $('.closeTR').toggle(0);">&nbsp;<img src="/component/sl_menu/resources//pictures/qs_option.png">&nbsp;</a>
+    <a class="floatLeft" href="javascript:;" onclick="$(this).closest('form').find('input:visible').val('').blur();">&nbsp;<img src="/component/form/resources/pictures/tree_clear.png" title="Clear quick search form" onclick="tp(this);">&nbsp;</a><a id="alt_submit" href="javascript:;" class="floatRight" onclick="
+          var asContainer = goTabs.create('candi', '', '', 'Candidate QS');
+          AjaxRequest('https://beta1.slate.co.jp/index.php5?uid=555-001&amp;ppa=ppasea&amp;ppt=candi&amp;ppk=0&amp;pg=ajx', 'body', 'quickSearchForm',  asContainer['id'], '', '', 'initHeaderManager(); ');
+          goTabs.select(asContainer['number']);">&nbsp;<img src="/component/search/resources/pictures/search_24.png"></a>
+          <input type="submit" style="opacity:0; width: 0px; height: 0px;">
+    </div><p class="floatHack"></p></td></tr></tbody></table>
+            </td>
+          </tr>
         </table>
       </td>
       <td valign="top" style='padding-left: 20px;'>
