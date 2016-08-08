5e0accb1780fada96c1c3209e45f026a09ccb0f5
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-07 19:54:45 -0700

    client side

--- component/display/resources/html/client_main_page.php
+++ component/display/resources/html/client_main_page.php
@@ -245,7 +245,7 @@ li{
         <tbody class='stayFixed workSpace'>
           <?php for($i=0;$i<22;$i++){ ?>
             <tr style='width:100%;'>
-              <td style='width:180px; border-bottom:1px solid #DADADA; padding-bottom: 5px;'>
+              <td style='width:180px; border-bottom:1px solid #DADADA; padding-bottom: 3px;padding-top: 3px;padding-left: 3px;'>
                 <a style=' font-size: 12px;' href="#" >My test folder (8)</a>
               </td>
             </tr>
