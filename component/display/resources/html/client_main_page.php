6a181af6068aa9eb791bd554e8dddc0eea0b4fba
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-08 19:51:05 -0700

    client side

--- component/display/resources/html/client_main_page.php
+++ component/display/resources/html/client_main_page.php
@@ -243,7 +243,7 @@ li{
           </tr>
           <tr>
             <td>
-              <select name="webmenu" id="webmenu">
+              <select style='width:190px !important;' name="webmenu" id="webmenu">
                 <option value="calendar" data-image="https://jobs.slate.co.jp//component/jobboard_user/resources/pictures/star.png">
                   Calendar
                 </option>
