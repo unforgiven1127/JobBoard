98513a57f671601f34f98f79e0deeee77aa87f02
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-17 21:32:51 -0700

    top menu

--- component/display/resources/html/client_header.php
+++ component/display/resources/html/client_header.php
@@ -18,7 +18,7 @@
         <table  align="center">
 		    <tr>
 		      <td >
-		        <img style="text-align: center; height: 45px; margin-right:50px; " src="/common/pictures/slate_logo.png" />
+		        <img style="text-align: center; height: 45px; margin-right:100px; " src="/common/pictures/slate_logo.png" />
 		      </td>
 		      <td class='topMenuText'>
 		      	<img style="text-align: center; height: 25px; margin-right:5px; " src="/common/pictures/home.png" />
