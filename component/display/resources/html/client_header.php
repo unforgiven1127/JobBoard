07649831438332e9e5dc596923ca5118d43f6fed
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-18 12:52:32 -0700

    top menu

--- component/display/resources/html/client_header.php
+++ component/display/resources/html/client_header.php
@@ -26,17 +26,17 @@
 		      	<img style="text-align: center; margin-left:10px; margin-right:5px;" src="/common/pictures/divider.png" />
 		      <td>
 		      <td class='topMenuText'>
-		      	<img style="text-align: center; height: 25px; margin-left:10px; margin-right:5px;" src="/common/pictures/home.png" />
+		      	<img style="text-align: center; height: 25px; margin-left:10px; margin-right:5px;" src="/common/pictures/add_48.png" />
 		      	<a style='margin-right:10px;font-size: 15px;'>Add </a>
 		      	<img style="text-align: center; margin-left:10px; margin-right:5px;" src="/common/pictures/divider.png" />
 		      <td>
 		      <td  class='topMenuText'>
-		      	<img  style="text-align: center; height: 25px; margin-left:10px; margin-right:5px;" src="/common/pictures/home.png" />
+		      	<img  style="text-align: center; height: 25px; margin-left:10px; margin-right:5px;" src="/common/pictures/stat_48.png" />
 		      	<a style='margin-right:10px;font-size: 15px;'>Reports </a>
 		      	<img style="text-align: center; margin-left:10px; margin-right:5px;" src="/common/pictures/divider.png" />
 		      <td>
 		      <td  class='topMenuText'>
-		      	<img  style="text-align: center; height: 25px; margin-left:10px;" src="/common/pictures/home.png" />
+		      	<img  style="text-align: center; height: 25px; margin-left:10px;" src="/common/pictures/tool_48.png" />
 		      	<a style='margin-right:10px;font-size: 15px;'>Settings </a>
 		      	<img style="text-align: center; margin-left:10px; margin-right:5px;" src="/common/pictures/divider.png" />
 		      <td>
