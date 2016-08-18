7ab480a85c7e6ffad8e5e2ef2df46a711cb2d4a1
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-18 15:02:13 -0700

    popup

--- component/display/resources/html/client_header.php
+++ component/display/resources/html/client_header.php
@@ -1,4 +1,11 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <style>
 	.topMenuText
 	{
