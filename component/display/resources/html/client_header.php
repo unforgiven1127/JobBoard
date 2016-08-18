c50f18d28a77e10d4246368e322a50e8de06ad98
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-17 20:47:04 -0700

    top menu

--- component/display/resources/html/client_header.php
+++ component/display/resources/html/client_header.php
@@ -1,10 +1,16 @@
 <table style='background-color: #E4E4E4; width: 100%;' align="center">
     <tr>
-      <td >
-        <img style="text-align: center; height: 45px; " src="/common/pictures/slistem_large.gif" />
+      <td align="center">
+        <table style='background-color: #E4E4E4; width: 100%;' align="center">
+		    <tr>
+		      <td >
+		        <img style="text-align: center; height: 45px; " src="/common/pictures/slistem_large.gif" />
+		      </td>
+		      <td style="color:#98321F;">
+		      	<img style="text-align: center; height: 45px; " src="/common/pictures/home.png" />Home
+		      <td>
+		    </tr>
+		</table>
       </td>
-      <td style="color:#98321F;">
-      	<img style="text-align: center; height: 45px; " src="/common/pictures/home.png" />Home
-      <td>
     </tr>
   </table>
