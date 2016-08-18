b2379e53cce02f044bff8a03a0ec30de8e5a3b45
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-18 14:56:04 -0700

    popup

--- component/display/resources/html/client_candi_page.php
+++ component/display/resources/html/client_candi_page.php
@@ -17,7 +17,7 @@
 <script>
 function pop_up(url){
   alert(url);
-  window.open(url,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=1076,height=768,directories=no,location=no');
+  window.open(url,'win2','status=no,toolbar=no,scrollbars=yes,titlebar=no,menubar=no,resizable=yes,width=1076,height=768,directories=no,location=no,screenX=0,screenY=100');
 }
 </script>
 
