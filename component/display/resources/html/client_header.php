2d286fa19eb0eb7f5754774989aa4900ad9c826d
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-17 20:57:16 -0700

    top menu

--- component/display/resources/html/client_header.php
+++ component/display/resources/html/client_header.php
@@ -1,3 +1,11 @@
+
+<style>
+	.topMenu
+	{
+		color:#98321F; font-size: 20px;
+	}
+</style>
+
 <table style='background-color: #E4E4E4; width: 100%;' align="center">
     <tr>
       <td align="center">
@@ -6,8 +14,9 @@
 		      <td >
 		        <img style="text-align: center; height: 45px; margin-right:10px; " src="/common/pictures/slistem_large.gif" />
 		      </td>
-		      <td style="color:#98321F; font-size: 20px;  ">
-		      	<img style="text-align: center; height: 25px; margin-right:10px;" src="/common/pictures/home.png" /><p>Home |</p>
+		      <td>
+		      	<img style="text-align: center; height: 25px; margin-right:10px;" src="/common/pictures/home.png" />
+		      	<b class='topMenu'>Home </b><b>|</b>
 		      <td>
 		    </tr>
 		</table>
