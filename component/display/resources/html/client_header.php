aa0d9d4a43318b05c5bf94b81522c9ce017fc4a8
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-18 12:54:15 -0700

    top menu

--- component/display/resources/html/client_header.php
+++ component/display/resources/html/client_header.php
@@ -21,22 +21,22 @@
 		        <img style="text-align: center; height: 45px; margin-right:245px; " src="/common/pictures/slate_logo.png" />
 		      </td>
 		      <td class='topMenuText'>
-		      	<img style="text-align: center; height: 25px; margin-right:5px; " src="/common/pictures/home_48.png" />
+		      	<img style="text-align: center; height: 30px; margin-right:5px; " src="/common/pictures/home_48.png" />
 		      	<a style='margin-right:10px;font-size: 15px;' >Home </a>
 		      	<img style="text-align: center; margin-left:10px; margin-right:5px;" src="/common/pictures/divider.png" />
 		      <td>
 		      <td class='topMenuText'>
-		      	<img style="text-align: center; height: 25px; margin-left:10px; margin-right:5px;" src="/common/pictures/add_48.png" />
+		      	<img style="text-align: center; height: 30px; margin-left:10px; margin-right:5px;" src="/common/pictures/add_48.png" />
 		      	<a style='margin-right:10px;font-size: 15px;'>Add </a>
 		      	<img style="text-align: center; margin-left:10px; margin-right:5px;" src="/common/pictures/divider.png" />
 		      <td>
 		      <td  class='topMenuText'>
-		      	<img  style="text-align: center; height: 25px; margin-left:10px; margin-right:5px;" src="/common/pictures/stat_48.png" />
+		      	<img  style="text-align: center; height: 30px; margin-left:10px; margin-right:5px;" src="/common/pictures/stat_48.png" />
 		      	<a style='margin-right:10px;font-size: 15px;'>Reports </a>
 		      	<img style="text-align: center; margin-left:10px; margin-right:5px;" src="/common/pictures/divider.png" />
 		      <td>
 		      <td  class='topMenuText'>
-		      	<img  style="text-align: center; height: 25px; margin-left:10px;" src="/common/pictures/tool_48.png" />
+		      	<img  style="text-align: center; height: 30px; margin-left:10px;" src="/common/pictures/tool_48.png" />
 		      	<a style='margin-right:10px;font-size: 15px;'>Settings </a>
 		      	<img style="text-align: center; margin-left:10px; margin-right:5px;" src="/common/pictures/divider.png" />
 		      <td>
