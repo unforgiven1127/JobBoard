6f2b24ad15a1ec94e82200a044670e1d9c9e389e
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-17 20:50:39 -0700

    top menu

--- component/display/resources/html/client_header.php
+++ component/display/resources/html/client_header.php
@@ -1,13 +1,13 @@
 <table style='background-color: #E4E4E4; width: 100%;' align="center">
     <tr>
       <td align="center">
-        <table style='background-color: #E4E4E4; width: 100%;' align="center">
+        <table  align="center">
 		    <tr>
 		      <td >
 		        <img style="text-align: center; height: 45px; " src="/common/pictures/slistem_large.gif" />
 		      </td>
 		      <td style="color:#98321F; font-size: 20px;">
-		      	<img style="text-align: center; height: 15px; margin-right:10px;" src="/common/pictures/home.png" />Home
+		      	<img style="text-align: center; height: 25px; margin-right:10px;" src="/common/pictures/home.png" />Home
 		      <td>
 		    </tr>
 		</table>
