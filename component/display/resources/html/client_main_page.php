dc852a4e88be2a190f8037fef75d9d40fb3ff77c
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-17 20:28:26 -0700

    top menu

--- component/display/resources/html/client_main_page.php
+++ component/display/resources/html/client_main_page.php
@@ -208,7 +208,7 @@ li{
     });
   </script>
 
-  <table style='background-color: #D0D0D0; width: 100%;' align="center">
+  <table style='background-color: #E4E4E4; width: 100%;' align="center">
     <tr>
       <td align="center">
         <div style="width: 300px;"><center><img style="text-align: center; width: 200px; " src="/common/pictures/slistem_large.gif" /></center></div>
