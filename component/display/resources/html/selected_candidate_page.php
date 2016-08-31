06d8ecc47e2ffcf8ae4a183a21fabaa0913087d4
unforgiven1127 <munir_anameric@hotmail.com>
2016-08-30 20:13:21 -0700

    client side select candi

--- component/display/resources/html/selected_candidate_page.php
+++ component/display/resources/html/selected_candidate_page.php
@@ -112,29 +112,35 @@ function pop_up(url, title, w, h) {
     <tr>
       <td valign="top" style='width: 385px;'>
         <table class='innerTableTopless' style='width: 385px; height: 420px;' >
-          <tr style='height: 20px !important;'>
-            <td valign="top" style='padding-top: 10px; width:80px; height: 20px !important; '><p class='titles'>Position :</p></td>
-            <td valign="top" style='padding-top: 10px; height: 20px !important;'><p>#8853 - test new position</p></td>
-          </tr>
           <tr>
-            <td style='height: 20px;'><p class='titles'>Status</p></td>
-            <td style='height: 20px;'><p>Meeting 1</p></td>
-          </tr>
-          <tr>
-            <td style='height: 20px;'><p class='titles'>Title</p></td>
-            <td style='height: 20px;'><p>Security Consultant</p></td>
-          </tr>
-          <tr>
-            <td style='height: 20px;'><p class='titles'>Occupation</p></td>
-            <td style='height: 20px;'><p>Consultant</p></td>
-          </tr>
-          <tr>
-            <td style='height: 20px;'><p class='titles'>Industry</p></td>
-            <td style='height: 20px;'><p>IT-Other</p></td>
-          </tr>
-          <tr>
-            <td style='height: 20px;'><p class='titles'>Last meeting</p></td>
-            <td style='height: 20px;'><p>2015-11-30</p></td>
+            <td>
+              <table>
+                <tr style='height: 20px !important;'>
+                  <td valign="top" style='padding-top: 10px; width:80px; height: 20px !important; '><p class='titles'>Position :</p></td>
+                  <td valign="top" style='padding-top: 10px; height: 20px !important;'><p>#8853 - test new position</p></td>
+                </tr>
+                <tr>
+                  <td style='height: 20px;'><p class='titles'>Status</p></td>
+                  <td style='height: 20px;'><p>Meeting 1</p></td>
+                </tr>
+                <tr>
+                  <td style='height: 20px;'><p class='titles'>Title</p></td>
+                  <td style='height: 20px;'><p>Security Consultant</p></td>
+                </tr>
+                <tr>
+                  <td style='height: 20px;'><p class='titles'>Occupation</p></td>
+                  <td style='height: 20px;'><p>Consultant</p></td>
+                </tr>
+                <tr>
+                  <td style='height: 20px;'><p class='titles'>Industry</p></td>
+                  <td style='height: 20px;'><p>IT-Other</p></td>
+                </tr>
+                <tr>
+                  <td style='height: 20px;'><p class='titles'>Last meeting</p></td>
+                  <td style='height: 20px;'><p>2015-11-30</p></td>
+                </tr>
+              </table>
+            </td>
           </tr>
         </table>
       </td>
